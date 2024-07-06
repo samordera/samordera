7c5ccb904af180d5381a6e0933878bdede6bb683
Dera <deraegbo@gmail.com>
2024-07-06 10:11:38 +0100

    Initialized repo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import '../app.postcss';
-
+	import { AppBar } from '@skeletonlabs/skeleton';
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
+<AppBar>
+
+</AppBar>
 <slot />
